19e187efc2bfc32a7f9197917df94e4d1dd0eed2
Nikita Bogachev <nikita.bogachyov@noorsoft.ru>
2021-12-05 14:43:17 +0500

    refactoring user router on koa.js

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,43 +1,69 @@
-const router = require('express').Router();
+const Router = require('koa-router');
 const User = require('./user.model');
 const usersService = require('./user.service');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  res.json(users.map(User.toResponse));
-});
+const router = new Router();
 
-router.route('/:id').get(async (req, res) => {
-  const { id } = req.params;
-  const user = await usersService.getUser(id);
-  if (user) {
-    res.json(User.toResponse(user));
-  } else {
-    res.sendStatus(404);
-  }
-});
+router
+  .get('/', async (ctx) => {
+    try {
+      const users = await usersService.getAll();
+      ctx.body = users.map(User.toResponse);
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+    }
+  })
+  .get('/:id', async (ctx) => {
+    try {
+      const { id } = ctx.params;
+      const user = await usersService.getUser(id);
+      if (user) {
+        ctx.status = 200;
+        ctx.body = User.toResponse(user);
+      } else {
+        ctx.status = 404;
+      }
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+    }
+  })
+  .post('/', async (ctx) => {
+    try {
+      const { name, login, password } = ctx.request.body;
+      const user = await usersService.createUser({ name, login, password });
+      ctx.status = 201;
+      ctx.body = User.toResponse(user);
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+    }
+  })
+  .put('/:id', async (ctx) => {
+    try {
+      const { id } = ctx.params;
+      const { body } = ctx.request;
+      const user = await usersService.updateUser(id, body);
+      ctx.body = User.toResponse(user);
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+    }
+  })
+  .delete('/:id', async (ctx) => {
+    try {
+      const { id } = ctx.params;
+      const result = await usersService.deleteUser(id);
+      if (result) {
+        ctx.status = 204;
+      } else {
+        ctx.status = 404;
+      }
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+    }
+  });
 
-router.route('/').post(async (req, res) => {
-  const { name, login, password } = req.body;
-  const user = await usersService.createUser({ name, login, password });
-  res.status(201).json(User.toResponse(user));
-});
-
-router.route('/:id').put(async (req, res) => {
-  const { id } = req.params;
-  const { body } = req;
-  const user = await usersService.updateUser(id, body);
-  res.json(User.toResponse(user));
-});
-
-router.route('/:id').delete(async (req, res) => {
-  const { id } = req.params;
-  const result = await usersService.deleteUser(id);
-  if (result) {
-    res.sendStatus(204);
-  } else {
-    res.status(404).json('Not Found');
-  }
-});
-
-module.exports = router;
+module.exports = () => router.routes();
