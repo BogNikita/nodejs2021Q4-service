cd0627f4cc7015e4880ab1bd45db6a17203284c2
Nikita Bogachev <nikita.bogachyov@noorsoft.ru>
2021-12-05 14:42:53 +0500

    refactoring task router on koa.js

--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -1,51 +1,76 @@
-const router = require('express').Router();
+const Router = require('koa-router');
 const tasksService = require('./task.service');
 
-router.route('/:boardId/tasks/').get(async (req, res) => {
-  const { boardId } = req.params;
-  const tasks = await tasksService.getAll(boardId);
-  res.json(tasks);
-});
+const router = new Router();
 
-router.route('/:boardId/tasks/:id').get(async (req, res) => {
-  const { id } = req.params;
-  const task = await tasksService.getTask(id);
-  if (task) {
-    res.json(task);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-router.route('/:boardId/tasks/').post(async (req, res) => {
-  const { boardId } = req.params;
-  const { title, order, description, userId, columnId } = req.body;
-  const task = await tasksService.createTask({
-    title,
-    order,
-    description,
-    userId,
-    boardId,
-    columnId,
+router
+  .get('/:boardId/tasks', async (ctx) => {
+    try {
+      const { boardId } = ctx.params;
+      const tasks = await tasksService.getAll(boardId);
+      ctx.body = tasks;
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+    }
+  })
+  .get('/:boardId/tasks/:id', async (ctx) => {
+    try {
+      const { id } = ctx.params;
+      const task = await tasksService.getTask(id);
+      if (task) {
+        ctx.body = task;
+      } else {
+        ctx.status = 404;
+      }
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+    }
+  })
+  .post('/:boardId/tasks', async (ctx) => {
+    try {
+      const { boardId } = ctx.params;
+      const { title, order, description, userId, columnId } = ctx.request.body;
+      const task = await tasksService.createTask({
+        title,
+        order,
+        description,
+        userId,
+        boardId,
+        columnId,
+      });
+      ctx.status = 201;
+      ctx.body = task;
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+    }
+  })
+  .put('/:boardId/tasks/:id', async (ctx) => {
+    try {
+      const { id } = ctx.params;
+      const { body } = ctx.request;
+      const task = await tasksService.updateTask(id, body);
+      ctx.body = task;
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+    }
+  })
+  .delete('/:boardId/tasks/:id', async (ctx) => {
+    try {
+      const { id } = ctx.params;
+      const result = await tasksService.deleteTask(id);
+      if (result) {
+        ctx.status = 204;
+      } else {
+        ctx.status = 404;
+      }
+    } catch (error) {
+      ctx.status = 500;
+      ctx.body = 'Internal server error';
+    }
   });
-  res.status(201).json(task);
-});
 
-router.route('/:boardId/tasks/:id').put(async (req, res) => {
-  const { id } = req.params;
-  const { body } = req;
-  const task = await tasksService.updateTask(id, body);
-  res.json(task);
-});
-
-router.route('/:boardId/tasks/:id').delete(async (req, res) => {
-  const { id } = req.params;
-  const result = await tasksService.deleteTask(id);
-  if (result) {
-    res.sendStatus(204);
-  } else {
-    res.status(404).json('Not Found');
-  }
-});
-
-module.exports = router;
+module.exports = () => router.routes();
