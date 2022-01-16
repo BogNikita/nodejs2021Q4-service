68e29d06be721d6824649cdf46f2c3a5e383c710
Nikita Bogachev <nikita.bogachyov@noorsoft.ru>
2022-01-16 12:22:10 +0500

    init typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 import CONFIG from './common/config';
 import app from './app';
+import ORMConfig from './ormconfig';
 
 const PORT = CONFIG.PORT || 4000;
 
 app.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
+
+(async () => {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await createConnection(ORMConfig);
+    console.log('Connect to db');
+  } catch (error) {
+    console.log(error);
+  }
+})();
